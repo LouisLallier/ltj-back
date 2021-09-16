9ebe862942b18f60b6d3a0fccaad043a6d9400dc
LouisLallier <louis.lallier@gmail.com>
2021-09-16 19:19:20 +0200

    crud user done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller');
 
-
+//auth
 router.post("/register", authController.signUp);
+// user display : 'block'
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
