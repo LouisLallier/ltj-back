bcae6c7afd5f6058865fc1907f3d8648b25e13b6
LouisLallier <louis.lallier@gmail.com>
2021-10-04 19:43:30 +0200

    token connexion OK Reponse Postman User ID

--- models/user.model.js
+++ models/user.model.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
 
         picture: {
             type: String,
-            // default: "path"
+            default: "../assets/default.jpg"
 
         },
         bio: {
@@ -44,12 +44,24 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-// play function before save into display:'block,
+// play function before save into display:'block',
 userSchema.pre("save", async function(next){
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email')
+};
 
 const UserModel = mongoose.model('user', userSchema);
 module.exports = UserModel;
