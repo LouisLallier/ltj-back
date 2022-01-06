5dab4687dcca1b40b6e753f3223cebc1f266e0d1
LouisLallier <louis.lallier@gmail.com>
2022-01-06 19:08:05 +0100

    CORS TEST * return to when allvideos worked

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -23,7 +23,7 @@ module.exports.checkUser = (req, res, next) => {
 };
 
 module.exports.requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt || req.headers["x-access-token"];
     try {
         if (token) {
             jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
